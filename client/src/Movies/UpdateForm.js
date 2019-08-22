50e30ce6fb61b8321fb1b0a7a2ce8b7b02dfca5c
denmercs <denmercs@gmail.com>
2019-08-22 16:29:06 -0500

    project

---
+++ client/src/Movies/UpdateForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+
+const initialMovie = {
+  id: "",
+  title: "",
+  director: "",
+  metascore: "",
+  stars: []
+};
+
+const UpdateForm = props => {
+  return (
+    <>
+      <h3>Testing</h3>
+    </>
+  );
+};
+
+export default UpdateForm;
